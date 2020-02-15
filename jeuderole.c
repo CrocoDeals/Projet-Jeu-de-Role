f766697fcb2b79c6f1143655f4b7c1b778884ca6
NoelieBoyer <59953453+NoelieBoyer@users.noreply.github.com>
2020-02-15 21:22:15 +0100

    Case2_ecouter_la_conversation
    + un petit bout de la suite (suivre le garde dans la salle informatique)

--- jeuderole.c
+++ jeuderole.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <windows.h>
+#include <unistd.h>
 
 #define MAX_CAPACITE 10
 
@@ -55,7 +56,9 @@ void afficherInventaire(joueur joueur) {
 }
 
 int main(){
+  color(15,0);
 	int choixAction;
+  int choixRecup;
 	joueur noelie={"Noelie",50,50,60,5,2,3,3,3,{{"Vetement Dechire","Vetement en lambeaux n'offrant aucune defense"},{"Morceau de Metal","Petit morceau de metal coupant"}},2,false};
 	lieu chapelle={1, "Chapelle en Ruine", "Chapelle servant de couverture a une operation de trafic de drogue de grande envergure"};
 	lieu egout={2, "Egout Obscur", "Egout servant d'entrepot de stockage pour la drogue, sert aussi de lien entre chaque batiment"};
@@ -66,36 +69,63 @@ int main(){
 		afficherInventaire(noelie);
 
 		do {
-		printf("\nVous avancez vers la lumiere et vous vous retrouvez dans une grande piece.\nPlusieurs caisses de stockage se trouve a droite.\nVous entendez des voix dans le couloir de gauche.\nQue faites-vous ? (1) Se cacher et fouiller les caisses  (2) Ecouter la conversation\n");
+		printf("\nVous avancez vers la lumiere et vous vous retrouvez dans une grande piece.\nPlusieurs caisses de stockage se trouve a droite.\nVous entendez des voix dans le couloir de gauche.\nQue faites-vous ? Se cacher et fouiller les caisses (1)  Ecouter la conversation (2)\n");
 		//color(15,0);
-			scanf("%d", choixAction);
+			scanf("%d", &choixAction);
 			// Premier choix du Joueur
 			switch (choixAction) {
 				// Se Cacher et Fouiller
 				case 1:
-            do{printf("Vous vous accroupissez derrière deux grosses caisses en bois et faites un trou dans le flan de la boite pour extirper son contenu.\n Vous trouvez une combinaison de laborantin.\n");
+            do{
+              printf("Vous vous accroupissez derriere une grosse caisse en bois et faites un trou avec votre morceau de metal pour extirper son contenu.\n Vous trouvez une combinaison de laborantin.\n");
             int choixRecup;
             printf("Voulez vous ramasser cet objet ? \n (1) Oui (2) Non\n");
             scanf("%d", &choixRecup);
             switch (choixRecup) {
               //objet dans inventaire
               case 1:
-              color (0,5);
-              printf("Vous obtenez combinaison de laborantin [defense + 1]\n");
-              noelie.def=noelie.def+1;
+
+                color (12,0);
+                printf("Vous obtenez combinaison de laborantin [defense + 1]\n");
+                noelie.def=noelie.def+1;
+                choixAction=0;
               break;
               //objet non récupéré
               case 2:
-              printf("  \n");
-              break;}
-            printf("Vous avez fait trop de bruit ! Les gardes qui discutaient non loin vous ont repéré !\n");}
+                printf("  \n");
+                choixAction=0;
+              break;
+            }
+            color (15,0);
+            printf("Vous avez fait trop de bruit ! Les gardes qui discutaient non loin vous ont repere !\n");
+          }
             while(choixAction != 0);
             choixAction = -1;
 					break;
 				// Ecouter la conversation
 				case 2:
 				do{
-				} while (choixAction != 0);
+          printf("-T'as entendu ca ? Apparament ils vont augmenter le taux de CTH dans l'hosmozine ! Les mules vont avoir du mal survivre pouahaha\n");
+          sleep (4);
+          printf("-Je trouve pas ca tres drole, ils pourraient etre plus professionels avec les mules, c'est pas comme s'ils disposaient des plus grands chirurgiens du monde\n");
+          sleep(4);
+          printf("-Rhoo ca va qu'est ce qu'on s'en fiche de savoir si ces gens meurent ou pas, dans tous les cas leurs vies etaient foutues autant qu'ils servent a quelque chose !\n");
+          sleep(4);
+          printf("-Tu me saoules, je vais aller me griller une cigarrette pour plus t'entendre dire des conneries\n");
+          sleep(4);
+          printf("-Tu feras gaffe, la sortie B6 est en maintenance il y a que la sortie E4 qui est accessible depuis ici : continue tout droit et tu tournes a la deuxieme a gauche\n");
+          sleep(4);
+          printf("-Ca marche moi je retourne en salle info inspecter les cameras de surveillance\n");
+          sleep(2);
+
+          printf("Vous decidez de suivre le garde jusqu'a la sortie mais la porte se referme devant vous, vous ne parvenez pas a vous echaper\n");
+          printf("Vous retournez sur vos pas et trouvez la salle informatique dont parlait l'autre garde\n Celui ci se trouve assis devant les écrans de surveillance avec un casque audio sur la tete\n");
+
+          printf("Que voulez vous faire ? Assomer le garde (1) Fouiller discrètement la salle (2)\n");
+          choixAction=0;
+
+          }
+				 while (choixAction != 0);
 				choixAction = -1;
 				default:;
 			}
